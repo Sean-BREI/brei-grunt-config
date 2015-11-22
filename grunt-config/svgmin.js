714d77c1aafaa835beab36f4aa66f1bb062b9935
Ian Moffitt <IMoffitt@barkleyrei.com>
2015-11-22 11:30:55 -0500

    Formatting upgrades. Added watch for fixtures, which resolves BarkleyREI/generator-brei-app#65

--- grunt-config/svgmin.js
+++ grunt-config/svgmin.js
@@ -1,13 +1,13 @@
 module.exports = function(grunt, options){
-  var yeoman = options.yeoman;
-  return {
-	  dist: {
-	    files: [{
-	      expand: true,
-	      cwd: yeoman.app + '/img',
-	      src: '{,*/}*.svg',
-	      dest: yeoman.dist + '/img'
-	    }]
-	  }
-	}
+	var yeoman = options.yeoman;
+	return {
+		dist: {
+			files: [{
+				expand: true,
+				cwd: yeoman.app + '/img',
+				src: '{,*/}*.svg',
+				dest: yeoman.dist + '/img'
+			}]
+		}
+	};
 };
