33a00c52f2705dbb5ecef6d69557db604e9a1618
Ian Moffitt <IMoffitt@barkleyrei.com>
2016-03-04 13:43:25 -0500

    Change execute path to new destination
    We are moving the updateScss.js file to a lib folder so that it is not
    run automatically every single time Assemble is called.

--- grunt-config/execute.js
+++ grunt-config/execute.js
@@ -2,7 +2,7 @@ module.exports = function(grunt, options){
 	var yeoman = options.yeoman;
 	return {
 		target: {
-			src: [yeoman.app + '/assemble/helpers/updateScss.js']
+			src: ['lib/updateScss.js']
 		}
 	};
 };
