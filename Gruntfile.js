1f125c02ad8fee75d664c5056dcd5208f7a93ad6
Ian Moffitt <IMoffitt@barkleyrei.com>
2016-04-11 13:01:11 -0400

    Add references to nconf and fs

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,6 +19,8 @@ module.exports = function (grunt) {
 	'use strict';
 
 	var configs = require('load-grunt-configs')(grunt, options);
+	var fs = require('fs');
+	var nconf = require('nconf');
 
 	// build a custom version of modernizr
 	grunt.loadNpmTasks('grunt-modernizr');
